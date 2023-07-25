9803ef5f498c7b3554bfc88f30b93374fa86fd39
Lester Arteaga <lesterarte@gmail.com>
2023-07-25 10:00:01 -0600

    add joi configuration

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,8 +1,6 @@
 export const EnvConfiguration = () => ({
   environment: process.env.NODE_DEV || 'dev',
-  mongodb:
-    process.env.MONGO_DB ||
-    'mongodb+srv://lesterarte:Us6u2pVVEM7ZMnXq@fulltimeforce.rztxln0.mongodb.net/fulltimeforce',
+  mongodb: process.env.MONGO_DB,
   port: process.env.PORT || 4000,
   defaultLimitPagination: process.env.DEFAULT_LIMIT_PAGINATION || 10,
 });
