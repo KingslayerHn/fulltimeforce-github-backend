5e9d791971e794ec77d4c3666f9096bb68bd30c5
Lester Arteaga <lesterarte@gmail.com>
2023-07-25 11:34:33 -0600

    first look create user and login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user-dto';
 import { LoginUserDto } from './dto/login-user-dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './interfaces';
 
 @Injectable()
 export class AuthService {
-  createUser(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      let user = await this.userModel.findOne({
+        email: createUserDto.email,
+      });
+
+      if (user) throw new BadRequestException('The email already exist');
+
+      user = await this.userModel.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+
+      return {
+        user,
+        token: this.getJwToken({
+          id: user.id,
+        }),
+      };
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException('The email already exist ');
+      }
+
+      throw new InternalServerErrorException();
+    }
   }
 
-  login(loginUserDto: LoginUserDto) {
-    return loginUserDto;
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) throw new UnauthorizedException('credential are not valid');
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('credential are not valid');
+
+    return {
+      user,
+      token: this.getJwToken({
+        id: user.id,
+      }),
+    };
+  }
+
+  private getJwToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 }
--- src/auth/dto/login-user-dto.ts
+++ src/auth/dto/login-user-dto.ts
@@ -1,9 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class LoginUserDto {
   @ApiProperty()
+  @IsString()
+  @IsEmail()
   email: string;
 
   @ApiProperty()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number and at least a special character',
+  })
   password: string;
 }
