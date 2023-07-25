bbe85b93c446ddb3f05b8c729e7e1a4ea83b2627
Lester Arteaga <lesterarte@gmail.com>
2023-07-25 11:11:15 -0600

    add JWT validation and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
 import { JoiValidationSchema } from './config/joi.validation';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './auth/entities/user.entity';
 
 @Module({
   imports: [
@@ -14,6 +15,12 @@ import { MongooseModule } from '@nestjs/mongoose';
       validationSchema: JoiValidationSchema,
     }),
     MongooseModule.forRoot(process.env.MONGO_DB),
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
     AuthModule,
   ],
   controllers: [AppController],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+    }),
+  ],
+  exports: [JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
